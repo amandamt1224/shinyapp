78b3de3ae84bd75a07191cf7ac9c7f268f807f92
amanda <amandamt1224@gmail.com>
2015-06-15 16:25:08 -0700

    few small tweaks

---
+++ sen_view.R
@@ -0,0 +1,10 @@
+library(ggplot2)
+tabPanel("Sen_View",
+         tabsetPanel(
+           
+           tabPanel("Sen_View"
+                    
+           ),
+           tabPanel("Help")
+         )
+)
--- ui.R
+++ ui.R
@@ -9,7 +9,7 @@ shinyUI(fluidPage(
     source('sen_over.R', local=TRUE)$value,
     source('sen_cntls.R', local=TRUE)$value,    
     source('sky_view.R', local=TRUE)$value,
-    tabPanel("Sen_View"),
+    source('sen_view.R', local=TRUE)$value,
     source('sen_data.R', local=TRUE)$value,
     
     widths = c(2,10)
